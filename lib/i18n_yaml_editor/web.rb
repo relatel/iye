98326a802f0d60dfe6da2277c574aa48ac426f9a
Harry Vangberg <harry@vangberg.name>
2012-08-30 20:03:36 +0200

    sort by locale as well

--- lib/i18n_yaml_editor/web.rb
+++ lib/i18n_yaml_editor/web.rb
@@ -24,7 +24,7 @@ module I18nYamlEditor
           options[:text] = /#{filters["text"]}/i if filters["text"].to_s.size > 0
           options[:complete] = false if filters["incomplete"] == "on"
           keys = app.store.filter_keys(options)
-          keys = keys.sort_by(&:key).group_by(&:key)
+          keys = keys.sort_by {|k| [k.key, k.locale]}.group_by(&:key)
           res.write view("translations.html", keys: keys, filters: filters)
         end
 
