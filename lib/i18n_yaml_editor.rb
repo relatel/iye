077ae2b478997e3741554770d495c52356d1941f
Harry Vangberg <harry@vangberg.name>
2012-08-31 11:38:14 +0200

    requires

--- lib/i18n_yaml_editor.rb
+++ lib/i18n_yaml_editor.rb
@@ -5,7 +5,9 @@ module I18nYamlEditor
 end
 
 require "i18n_yaml_editor/app"
+require "i18n_yaml_editor/category"
 require "i18n_yaml_editor/key"
 require "i18n_yaml_editor/store"
 require "i18n_yaml_editor/transformation"
+require "i18n_yaml_editor/translation"
 require "i18n_yaml_editor/web"
