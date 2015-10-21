relatel/iye

commit a5a7bcd0d862a0482ebcd163f6ac61a9f55340fc

DRYed up App instantiation

lib/i18n_yaml_editor/app.rb
@@ -15,6 +15,8 @@ module I18nYamlEditor
       @rel_path = path
       @full_path = File.expand_path(path, @base_path)
       @store = Store.new
+
+      load_translations
     end
 
     def load_translations

lib/i18n_yaml_editor/store.rb
@@ -101,6 +101,7 @@ module I18nYamlEditor
         translation = Translation.new(name: name, text: text, file: file)
         add_translation(translation)
       }
+      create_missing_keys
     end
 
     def to_yaml

bin/iye
@@ -10,13 +10,7 @@ end
 require 'i18n_yaml_editor'
 require 'rack'
 
-iye = I18nYamlEditor::App.new(ARGV[0])
-
-$stdout.puts " * Loading translations from #{iye.full_path}"
-iye.load_translations
-
-$stdout.puts " * Creating missing translations"
-iye.store.create_missing_keys
-
+iye_app = I18nYamlEditor::App.new(source_folder)
+$stdout.puts " * Loading translations from #{iye_app.full_path}"
 $stdout.puts " * Starting web editor at port 5050"
-Rack::Server.start :app => I18nYamlEditor::Web.app_stack(iye), :Port => 5050
+Rack::Server.start :app => I18nYamlEditor::Web.app_stack(iye_app), :Port => 5050

config.ru
@@ -1,8 +1,7 @@
 require 'i18n_yaml_editor'
 
-iye_app = I18nYamlEditor::App.new('example')
-iye_app.load_translations
-iye_app.store.create_missing_keys
-
 # mounted on path as (rails)-integration example and for testing
-map('/dev/iye') { run I18nYamlEditor::Web.app_stack(iye_app) }
+map('/dev/iye') {
+  iye_app = I18nYamlEditor::App.new('example')
+  run I18nYamlEditor::Web.app_stack(iye_app)
+}

commit bc98101c7f69131158e21793272ef10b411fd461

Fix regression introduced by a5a7bcd0d862a0482ebcd163f6ac61a9f55340fc

lib/i18n_yaml_editor/app.rb
@@ -17,6 +17,7 @@ module I18nYamlEditor
       @store = Store.new
 
       load_translations
+      store.create_missing_keys
     end
 
     def load_translations

lib/i18n_yaml_editor/store.rb
@@ -97,7 +97,6 @@ module I18nYamlEditor
         translation = Translation.new(name: name, text: text, file: file)
         add_translation(translation)
       }
-      create_missing_keys
     end
 
     def to_yaml